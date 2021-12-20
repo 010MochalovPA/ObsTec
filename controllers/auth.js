e27927e510cbc8b7b84205b4051d29edc722c15d
Павел Мочалов <010mochalovpa@gmail.com>
2021-12-20 19:51:30 +0300

    add jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,37 @@
-const bycript = require("bcryptjs");
-const bcrypt = require("bcryptjs/dist/bcrypt");
-
+const bcript = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const keys = require("../config/keys");
 
 module.exports.login = async (request, response) => {
-  response.status(200).json({
-    login: {
-      username: request.body.username,
-      password: request.body.password,
-    },
-  });
+  // username: String;
+  // password: String;
+  // expiresIn: String
+  tmpUser = await User.findOne({ username: request.body.username });
+  if (tmpUser) {
+    const passwordResult = bcript.compareSync(request.body.password, tmpUser.password);
+    if (passwordResult) {
+      const token = jwt.sign(
+        {
+          username: tmpUser.username,
+          userId: tmpUser._id,
+        },
+        keys.jwt,
+        { expiresIn: 60 * request.body.expiresIn }
+      ); //expiresIn время жизни токена
+
+      response.status(200).json({ token: `Bearer ${token}` });
+    } else {
+      response.status(401).json({ message: "Пароль введен не верно" });
+    }
+  } else {
+    response.status(404).json({ message: "Пользователь не найден" });
+  }
 };
 
 module.exports.register = async (request, response) => {
-  // username password
+  // username: String;
+  // password: String;
 
   if (await User.findOne({ username: request.body.username })) {
     console.log("Пользователь уже существует");
