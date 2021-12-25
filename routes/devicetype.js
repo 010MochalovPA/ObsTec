7d392cd4b96f9e775a3be5d080a534eb59ebd912
Павел Мочалов <010mochalovpa@gmail.com>
2021-12-25 20:50:59 +0300

    create routing module and proxi

--- routes/devicetype.js
+++ routes/devicetype.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/devicetype");
+const passport = require("passport");
 
 //localhost:5000/api/devicetype
-router.get("/", controller.getAll);
+router.get("/", passport.authenticate("jwt", { session: false }), controller.getAll);
 
 //localhost:5000/api/devicetype/:id
-router.get("/:id", controller.getById);
+router.get("/:id", passport.authenticate("jwt", { session: false }), controller.getById);
 
 //localhost:5000/api/devicetype/:id
-router.delete("/:id", controller.delete);
+router.delete("/:id", passport.authenticate("jwt", { session: false }), controller.delete);
 
 //localhost:5000/api/devicetype/
-router.post("/", controller.create);
+router.post("/", passport.authenticate("jwt", { session: false }), controller.create);
 
 //localhost:5000/api/devicetype/:id
-router.patch("/:id", controller.update);
+router.patch("/:id", passport.authenticate("jwt", { session: false }), controller.update);
 
 module.exports = router;
--- routes/person.js
+++ routes/person.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/person");
+const passport = require("passport");
 
 //localhost:5000/api/person
-router.get("/", controller.getAll);
+router.get("/", passport.authenticate("jwt", { session: false }), controller.getAll);
 
 //localhost:5000/api/person
-router.post("/", controller.create);
+router.post("/", passport.authenticate("jwt", { session: false }), controller.create);
 
 //localhost:5000/api/person/:id
-router.get("/:id", controller.getById);
+router.get("/:id", passport.authenticate("jwt", { session: false }), controller.getById);
 
 //localhost:5000/api/person/:id
-router.delete("/:id", controller.delete);
+router.delete("/:id", passport.authenticate("jwt", { session: false }), controller.delete);
 
 //localhost:5000/api/person/:id
-router.patch("/:id", controller.update);
+router.patch("/:id", passport.authenticate("jwt", { session: false }), controller.update);
 
 module.exports = router;
